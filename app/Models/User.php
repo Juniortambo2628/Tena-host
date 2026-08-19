0cdfd435543ec9a89ede7d9c2093d070e58912fc
Juniortambo2628 <100776648+Juniortambo2628@users.noreply.github.com>
2026-08-19 12:20:54 +0000

    style: auto-fix PHP code style with Laravel Pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,9 +10,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Cache;
-use Laravel\Sanctum\HasApiTokens;
 use Laravel\Passkeys\Contracts\PasskeyUser;
 use Laravel\Passkeys\PasskeyAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements PasskeyUser
 {
